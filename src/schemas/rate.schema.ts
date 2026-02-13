9b534ab62fef7d8ebc984e6263fc6a96e2b0237a
Nguyen Nhat Quan <22644951.quan@student.iuh.edu.vn>
2026-02-13 23:31:00 +0700

    23:30 13/2/2026

--- src/schemas/rate.schema.ts
+++ src/schemas/rate.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createRateSchema = z.object({
   movie_id: z.string().min(1, "ID phim là bắt buộc"),
   user_id: z.string().min(1, "ID người dùng là bắt buộc"),
-  stars: z.number().min(1, "Đánh giá phải ít nhất 1 sao").max(10, "Đánh giá không thể vượt quá 10 sao"),
+  stars: z.number().min(1, "Đánh giá phải ít nhất 1 sao").max(5, "Đánh giá không thể vượt quá 5 sao"),
   created_at: z.string().default(new Date(Date.now()).toISOString()).optional(),
 });
 
